1c5a1452810106f37ac6a8f2183d94d97d6fd158
Thomas Cazade <cazade.thomas@gmail.com>
2022-06-27 20:38:49 +0200

    Add Metropolis font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
+
   theme: {
     extend: {
       fontFamily: {
-        inter: ["Inter", "sans-serif"],
+        inter: ["Inter", ...defaultTheme.fontFamily.sans],
+        metropolis: ["Metropolis", ...defaultTheme.fontFamily.sans],
       },
     },
   },
+
   plugins: [],
 }
