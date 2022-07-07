d096432169953a1a0a457e9066b4f44f06ba5ce9
Thomas Cazade <cazade.thomas@gmail.com>
2022-07-07 18:07:50 +0200

    Add Cypress tests for RegisterForm

---
+++ cypress/e2e/register.cy.ts
@@ -0,0 +1,50 @@
+describe("register view specs", () => {
+  it("should display the register form", () => {
+    cy.visit("/");
+    cy.contains("button", "Create an account").click();
+    cy.contains("button", "Register");
+  });
+
+  it("can't create an account if the fields are empty", () => {
+    cy.visit("/");
+    cy.contains("button", "Create an account").click();
+    cy.contains("button", "Register").click();
+    cy.contains("p", "This field is required.");
+  });
+
+  it("can't create an account if secret-key is invalid", () => {
+    cy.intercept("/api/user/register", {
+      statusCode: 401,
+      body: {},
+    });
+
+    cy.visit("/");
+
+    cy.contains("button", "Create an account").click();
+    cy.get("input#username").type("username");
+    cy.get("input#email").type("test@example.com");
+    cy.get("input#password").type("azerty123");
+    cy.get("input#secret-key").type("50e0172aea0b");
+    cy.contains("button", "Register").click();
+    cy.contains("p", "The provided secret-key is invalid.");
+  });
+
+  it("can create an account if all the fields are filled", () => {
+    cy.intercept("/api/user/register", {
+      statusCode: 200,
+      fixture: "login.json",
+    });
+
+    cy.visit("/");
+
+    cy.contains("button", "Create an account").click();
+    cy.get("input#username").type("username");
+    cy.get("input#email").type("test@example.com");
+    cy.get("input#password").type("azerty123");
+    cy.get("input#secret-key").type("50e0172aea0b");
+    cy.contains("button", "Register").click();
+    cy.location().should((location) => {
+      expect(location.pathname).to.eq("/audio-player");
+    });
+  });
+});
