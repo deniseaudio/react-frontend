1455d1ecff199fd92b2ebd00a85e805c09e87c18
Thomas Cazade <cazade.thomas@gmail.com>
2022-07-08 18:55:13 +0200

    Add variant on Button component, fix missing button props destructuring

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,10 +1,30 @@
-export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
+import cx from "classnames";
 
-export const Button: React.FC<ButtonProps> = ({ type, children }) => {
+export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: "primary" | "secondary";
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  variant = "primary",
+  type,
+  children,
+  className,
+  ...buttonProps
+}) => {
   return (
     <button
       type={type === "button" ? "button" : "submit"}
-      className="block w-full rounded-xl bg-green-600 px-6 py-3 text-sm font-semibold uppercase tracking-wider text-white ring-green-600 ring-opacity-40 transition-colors duration-200 ease-in-out hover:bg-green-500 focus:outline-none focus:ring-2 disabled:cursor-not-allowed disabled:opacity-70"
+      className={cx(
+        "block w-full rounded-xl px-6 py-3 text-sm font-semibold uppercase tracking-wider ring-opacity-40 transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 disabled:cursor-not-allowed disabled:opacity-70",
+        variant === "primary"
+          ? "bg-green-600 text-white ring-green-600 hover:bg-green-500"
+          : null,
+        variant === "secondary"
+          ? " bg-neutral-700 text-neutral-200 ring-neutral-700 hover:bg-neutral-600"
+          : null,
+        className
+      )}
+      {...buttonProps}
     >
       {children}
     </button>
