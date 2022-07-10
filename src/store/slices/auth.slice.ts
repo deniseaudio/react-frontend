adcd8b5d66bdf74ad6d08aacb4cba3297bd66509
Thomas Cazade <cazade.thomas@gmail.com>
2022-07-10 09:07:57 +0200

    Reset history on logout

--- src/store/slices/auth.slice.ts
+++ src/store/slices/auth.slice.ts
@@ -21,5 +21,6 @@ export const createAuthSlice: StoreSlice<AuthSlice> = (set) => ({
       isSongLoading: false,
       songLoadingProgression: null,
       likes: [],
+      history: [],
     })),
 });
