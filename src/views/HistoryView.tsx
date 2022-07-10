25079bf5367bd64324a3ebe277d789f193f2fdc3
Thomas Cazade <cazade.thomas@gmail.com>
2022-07-10 09:12:03 +0200

    Add text when no listening history

--- src/views/HistoryView.tsx
+++ src/views/HistoryView.tsx
@@ -17,16 +17,24 @@ export const HistoryView: React.FC = () => {
           Session history
         </p>
 
-        <div className="flex flex-col">
-          {history.map((song, index) => (
-            <SongItem
-              key={song.id}
-              song={song}
-              index={index}
-              onLikeUpdate={onLikeUpdate}
-            />
-          ))}
-        </div>
+        {history.length === 0 ? (
+          <p className="text-center font-metropolis text-lg font-medium leading-tight text-neutral-50">
+            No listening history, go listen to some songs!
+          </p>
+        ) : null}
+
+        {history.length > 0 ? (
+          <div className="flex flex-col">
+            {history.map((song, index) => (
+              <SongItem
+                key={song.id}
+                song={song}
+                index={index}
+                onLikeUpdate={onLikeUpdate}
+              />
+            ))}
+          </div>
+        ) : null}
       </div>
     </>
   );
